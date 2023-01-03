d55ca52908a89f2a3a8798b0ab9f3f16c8a0fa66
Aleksandar <32557966+salecivija@users.noreply.github.com>
2023-01-03 10:03:40 +0100

    add GUI + save JSON

--- main.py
+++ main.py
@@ -1,32 +1,68 @@
 #!/usr/bin/env python
 
-# Import the required libraries
 import re
 import json
+import tkinter as tk
+from tkinter import filedialog
+import subprocess
 
-# Open the M3U file in read mode
-with open('file.m3u', 'r') as f:
+def parse_m3u():
+  # Open the M3U file in read mode
+  f = open(file_path.get(), 'r', encoding='utf-8')
+
   # Read the contents of the file into a list
   lines = f.readlines()
 
-# Initialize an empty list for the data
-data = []
+  # Close the file
+  f.close()
 
-# Iterate through the lines in the file
-for line in lines:
-  # Check if the line starts with '#EXTINF:'
-  if line.startswith('#EXTINF:'):
-    # Split the line by ',' and get the name of the channel
-    name = line.split(',')[1]
-    # Get the next line, which is the URL of the channel
-    url = next(f).strip()
-    # Add the name and URL to the data list
-    data.append((name, url))
+  # Initialize an empty list for the data
+  data = []
 
-# Serialize the data list as a JSON array
-json_data = json.dumps(data)
+  # Iterate through the lines in the file
+  for line in lines:
+    # Check if the line starts with '#EXTINF:'
+    if line.startswith('#EXTINF:'):
+      # Split the line by ',' and get the name of the channel
+      name = line.split(',')[1]
+      # Get the next line, which is the URL of the channel
+      url = next(f).strip()
+      # Add the name and URL to the data list
+      data.append((name, url))
 
-# Open a file for writing
-with open('data.json', 'w') as f:
-  # Write the JSON array to the file
-  f.write(json_data)
+  # Serialize the data list as a JSON array
+  json_data = json.dumps(data)
+
+  # Open a temporary file for writing
+  with open('temp.json', 'w') as f:
+    # Write the JSON array to the file
+    f.write(json_data)
+
+  # Open the file in Notepad
+  subprocess.run(['notepad.exe', 'temp.json'])
+
+# Create the GUI window
+root = tk.Tk()
+root.title('M3U Parser')
+root.geometry('320x240')
+
+# Create a label
+label = tk.Label(root, text='Select an M3U file:')
+label.pack()
+
+# Create a text entry field
+# Create a text entry field to store the file path
+file_path = tk.StringVar()
+entry = tk.Entry(root, textvariable=file_path)
+entry.pack()
+
+# Create a button to open the file selection dialog
+button = tk.Button(root, text='Browse', command=lambda: file_path.set(filedialog.askopenfilename()))
+button.pack()
+
+# Create a button to parse the M3U file
+parse_button = tk.Button(root, text='Parse M3U', command=parse_m3u)
+parse_button.pack()
+
+# Run the GUI loop
+root.mainloop()
